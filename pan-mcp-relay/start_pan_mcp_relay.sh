8d7a52e6af441c6c62f72dbaa4f405be0cad9863
leonhanl <15275800+leonhanl@users.noreply.github.com>
2025-11-24 19:46:14 +0800

    fix: add shebang and log cleanup in start_pan_mcp_relay script

--- pan-mcp-relay/start_pan_mcp_relay.sh
+++ pan-mcp-relay/start_pan_mcp_relay.sh
@@ -1,1 +1,3 @@
-uvx pan-mcp-relay --config-file ./mcp-relay.yaml --transport=http -H 0.0.0.0 -p 8800 -d
+#!/bin/bash
+rm -f mcp_relay.log
+uvx pan-mcp-relay --config-file ./mcp-relay.yaml --transport=http -H 0.0.0.0 -p 8800 -d 2>&1 | tee mcp_relay.log
